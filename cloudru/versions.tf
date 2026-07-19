2f257701b458a1517817d73596558b7fa99c4da5
Konstantin Makarenko <konstantinmakarenko6064@gmail.com>
2026-07-19 10:58:12 +0600

    cloudru fix

--- cloudru/versions.tf
+++ cloudru/versions.tf
@@ -6,13 +6,19 @@ terraform {
   required_providers {
     cloudru = {
       source  = "cloud.ru/cloudru/cloud"
-      version = "2.0.2"
+      version = "2.0.0"
     }
   }
 }
 
 provider "cloudru" {
+  project_id  = var.project_id
   auth_key_id = var.auth_key_id
   auth_secret = var.auth_secret
-  project_id  = var.project_id
+
+  endpoints = {
+    iam_endpoint     = "iam.api.cloud.ru:443"
+    vpc_endpoint     = "vpc.api.cloud.ru:443"
+    compute_endpoint = "compute.api.cloud.ru:443"
+  }
 }
